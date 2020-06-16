ff681565ff85745ad855b1ca326f42dc836a8226
hon454 <hon454@naver.com>
2020-06-16 18:54:55 +0900

    회원 가입 엑티비티에서 백 버튼 누를 시 로그인 화면으로 전환되도록 변경

--- app/src/main/java/com/hon454/matchup/LoginActivity.java
+++ app/src/main/java/com/hon454/matchup/LoginActivity.java
@@ -2,40 +2,60 @@ package com.hon454.matchup;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.hon454.matchup.Database.User;
 
-public class LoginActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
-    private FirebaseAuth mAuth; // Declare Firebase Auth
+public class LoginActivity extends BaseActivity {
+
+    private static final String TAG = "LoginActivity";
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mUserDatabaseReference;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+//        mUserDatabaseReference = getUserDatabaseReference();
+//        ValueEventListener userListener = new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//                Log.w(TAG, "loadUser:onCancelled", databaseError.toException());
+//            }
+//        };
+
+
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
-
+        Log.d(TAG, mAuth.getUid() + " / " + mAuth.getCurrentUser());
         //한 번 로그인 하고 나중에 어플리케이션을 켰을 때 로그인 화면 스킵
         if (mAuth.getCurrentUser() != null) {
+
             Intent intent = new Intent(getApplication(), MainActivity.class);
             startActivity(intent);
             finish();
@@ -75,7 +95,6 @@ public class LoginActivity extends AppCompatActivity {
                                 // If sign in fails, display a message to the user.
                                 Toast.makeText(getApplicationContext(), "Authentication failed.",
                                         Toast.LENGTH_SHORT).show();
-                                updateUI(null);
                             }
                             // ...
                         }
@@ -86,14 +105,11 @@ public class LoginActivity extends AppCompatActivity {
     private void signUp() {
         Intent intent = new Intent(this, SignUpActivity.class);
         startActivity(intent);
-        finish();
     }
 
     private void updateUI(FirebaseUser user) {
-        if (user != null) {
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
